98e70abf5206cc580703939d2707125978fa6c72
SANTIAGO SANCHEZ OSPINA <santiagosanchezospina@192.168.2.13>
2024-09-24 22:54:56 -0500

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,8 @@
+URL_SERVICE = 'https://cnt-e0adb03e-368e-4fff-b51f-93fc347e3195.containerhub.tripleten-services.com'
+DOC_PATH = '/docs/'
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
+
+
